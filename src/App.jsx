4c6e9401b767db5685c10e57440752b274a409e7
Gowtham <gowthamrajendran1996@gmail.com>
2025-06-13 12:02:14 +0530

    ajcarcare frontend setup  - comming soon

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="rotate-container">
+      <h1 className="rotating">Aj Car Care Coming Soon</h1>
+    </div>
+  );
+}
+
+export default App;
