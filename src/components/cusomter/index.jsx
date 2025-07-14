c2885ac2ca6e03590f92a092017ee7655da917bd
Gowtham <gowthamrajendran1996@gmail.com>
2025-07-14 15:14:43 +0530

    Gowtham - sort customer by blocknumber

--- src/components/cusomter/index.jsx
+++ src/components/cusomter/index.jsx
@@ -20,122 +20,122 @@ import { useParams } from "react-router-dom";
 import CustomerModal from "./customerModal";
 import CustomerSubscriptionModal from "./customerSubscriptionModal";
 const Customer = () => {
-    const dispatch = useDispatch();
-    const { list, loading } = useSelector((state) => state.customer);
-    const [showModal, setShowModal] = useState({open: false, data: null})
-    const [showEditModal, setShowEditModal] = useState({ open: false, data: null });
-    const [showCustomerSubModal, setShowCustomerSubModal] = useState({ open: false, data: null});
+  const dispatch = useDispatch();
+  const { list, loading } = useSelector((state) => state.customer);
+  const [showModal, setShowModal] = useState({ open: false, data: null })
+  const [showEditModal, setShowEditModal] = useState({ open: false, data: null });
+  const [showCustomerSubModal, setShowCustomerSubModal] = useState({ open: false, data: null });
 
-    const { id } = useParams();
-    useEffect(() => {
-        if (id) {
-          dispatch(fetchCustomer(id));
-        }
-      }, [dispatch, id]);
-    return (
-        <>
-          {/* Main Content */}
-          <Box sx={{ p: 2 }}>
-            <Box sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}>
-              <Typography variant="h5">Customers</Typography>
-              <Button
-                variant="contained"
-                onClick={() => setShowModal({
-                  open: true, data: {
-                    apartmentId: id
-                  }
-                })}
-              >
-                Add Customers
-              </Button>
-            </Box>
-    
-            <TableContainer component={Paper} elevation={3}>
-              <Table>
-                <TableHead sx={{ backgroundColor: "#f5f5f5" }}>
-                  <TableRow>
-                    <TableCell><strong>#</strong></TableCell>
-                    <TableCell><strong>Name</strong></TableCell>
-                    <TableCell><strong>Phone</strong></TableCell>
-                    <TableCell><strong>Block No.</strong></TableCell>
-                    <TableCell><strong>Flat No.</strong></TableCell>
-                    <TableCell><strong>Action</strong></TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                {loading ? (
+  const { id } = useParams();
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchCustomer(id));
+    }
+  }, [dispatch, id]);
+  return (
+    <>
+      {/* Main Content */}
+      <Box sx={{ p: 2 }}>
+        <Box sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}>
+          <Typography variant="h5">Customers</Typography>
+          <Button
+            variant="contained"
+            onClick={() => setShowModal({
+              open: true, data: {
+                apartmentId: id
+              }
+            })}
+          >
+            Add Customers
+          </Button>
+        </Box>
+
+        <TableContainer component={Paper} elevation={3}>
+          <Table>
+            <TableHead sx={{ backgroundColor: "#f5f5f5" }}>
+              <TableRow>
+                <TableCell><strong>#</strong></TableCell>
+                <TableCell><strong>Name</strong></TableCell>
+                <TableCell><strong>Phone</strong></TableCell>
+                <TableCell><strong>Block No.</strong></TableCell>
+                <TableCell><strong>Flat No.</strong></TableCell>
+                <TableCell><strong>Action</strong></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {loading ? (
                 <TableRow>
-                  <TableCell colSpan={7} align="center">
+                  <TableCell colSpan={6} align="center">
                     <CircularProgress size={28} />
                   </TableCell>
                 </TableRow>
               ) : list.length > 0 ? (
-                    list.map((customer, index) => (
-                      <TableRow key={customer._id}>
-                        <TableCell>{index + 1}</TableCell>
-                        <TableCell>{customer.name}</TableCell>
-                        <TableCell>{customer.phone}</TableCell>
-                        <TableCell>{customer.blockNumber}</TableCell>
-                        <TableCell>{customer.flatNumber}</TableCell>
-                        <TableCell>
-                          <IconButton color="primary" 
-                            onClick={() => {
-                              setShowEditModal({
-                                open: true,
-                                data: customer
-                              })
-                            }}
-                          >
-                            <EditNoteOutlined />
-                          </IconButton>
-                          <IconButton color="primary" onClick={() => {
-                            setShowCustomerSubModal({
-                              open: true,
-                              data: customer
-                            })
-                          }}
-                          >
-                            <ManageAccounts />
-                          </IconButton>
-                        </TableCell>
-                      </TableRow>
-                    ))
-                  ): (
-                    <TableRow>
-                      <TableCell colSpan={7} align="center">
-                        No Customers found.
+                [...list]
+                  .sort((a, b) => a.blockNumber.localeCompare(b.blockNumber))
+                  .map((customer, index) => (
+                    <TableRow key={customer._id}>
+                      <TableCell>{index + 1}</TableCell>
+                      <TableCell>{customer.name}</TableCell>
+                      <TableCell>{customer.phone}</TableCell>
+                      <TableCell>{customer.blockNumber}</TableCell>
+                      <TableCell>{customer.flatNumber}</TableCell>
+                      <TableCell>
+                        <IconButton
+                          color="primary"
+                          onClick={() =>
+                            setShowEditModal({ open: true, data: customer })
+                          }
+                        >
+                          <EditNoteOutlined />
+                        </IconButton>
+                        <IconButton
+                          color="primary"
+                          onClick={() =>
+                            setShowCustomerSubModal({ open: true, data: customer })
+                          }
+                        >
+                          <ManageAccounts />
+                        </IconButton>
                       </TableCell>
                     </TableRow>
-                  )}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Box>
-    
-          {showModal.open && <CustomerModal
-            data={showModal.data}
-            open={showModal.open}
-            handleClose={() => setShowModal({ open: false, data: null })}
-          />}
-          {
-            showEditModal.open && <CustomerModal
-              data={showEditModal.data}
-              open={showEditModal.open}
-              editModal={true}
-              handleClose={() => setShowEditModal({ open: false, data: null})}
-            />
-          }
-          {
-            showCustomerSubModal.open && <CustomerSubscriptionModal
-            data={showCustomerSubModal.data}
-            open={showCustomerSubModal.open}
-            handleClose={() => {
-              setShowCustomerSubModal({ open: false, data: null});
-            }}
-            />
-          }
-        </>
-      );
+                  ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={6} align="center">
+                    No Customers found.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+      </Box>
+
+      {showModal.open && <CustomerModal
+        data={showModal.data}
+        open={showModal.open}
+        handleClose={() => setShowModal({ open: false, data: null })}
+      />}
+      {
+        showEditModal.open && <CustomerModal
+          data={showEditModal.data}
+          open={showEditModal.open}
+          editModal={true}
+          handleClose={() => setShowEditModal({ open: false, data: null })}
+        />
+      }
+      {
+        showCustomerSubModal.open && <CustomerSubscriptionModal
+          data={showCustomerSubModal.data}
+          open={showCustomerSubModal.open}
+          handleClose={() => {
+            setShowCustomerSubModal({ open: false, data: null });
+          }}
+        />
+      }
+    </>
+  );
 };
 
 export default Customer;
